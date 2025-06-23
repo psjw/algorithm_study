a8e36ea3dee097be01daa9616500cb1deaa81ba0
psjw <dlrudtn108@gmail.com>
2025-06-23 19:07:28 +0900

    feat(algorithm): [프로그래머스 68644] 두 개 뽑아서 더하기

---
+++ programmers/src/book/코딩테스트합격자되기/두개뽑아서더하기.java
@@ -0,0 +1,24 @@
+package book.코딩테스트합격자되기;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class 두개뽑아서더하기 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{2,1,3,4,1})));
+        System.out.println(Arrays.toString(solution(new int[]{5,0,2,7})));
+    }
+    private static int[] solution(int[] numbers) {
+        int[] answer = {};
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                set.add(numbers[i]+numbers[j]);
+            }
+        }
+
+        answer = set.stream().mapToInt(x -> x).sorted().toArray();
+        return answer;
+    }
+}
