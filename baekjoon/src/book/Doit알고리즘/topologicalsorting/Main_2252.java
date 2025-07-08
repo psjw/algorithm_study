7795145b9d52abe823ac90caaec015a8a0c6d42f
psjw <dlrudtn108@gmail.com>
2025-07-08 20:31:39 +0900

    feat(algorithm): [백준 2252] 줄 세우기 스페셜 저지

---
+++ baekjoon/src/book/Doit알고리즘/topologicalsorting/Main_2252.java
@@ -0,0 +1,54 @@
+package book.Doit알고리즘.topologicalsorting;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_2252 {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            ArrayList<Integer>[] graph = new ArrayList[N + 1];
+            for (int i = 1; i <= N; i++) {
+                graph[i] = new ArrayList<>();
+            }
+
+            int indegree[] = new int[N + 1];
+
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                graph[a].add(b);
+                indegree[b]++;
+            }
+
+
+            Queue<Integer> queue = new LinkedList<>();
+            for (int i = 1; i <= N; i++) {
+                if (indegree[i] == 0) queue.add(i);
+            }
+
+            List<Integer> result = new ArrayList<>();
+            while (!queue.isEmpty()) {
+                int cur = queue.poll();
+                result.add(cur);
+                for (int i = 0; i < graph[cur].size(); i++) {
+                    int next = graph[cur].get(i);
+                    indegree[next]--;
+                    if (indegree[next] == 0) {
+                        queue.add(next);
+                    }
+                }
+            }
+
+            for(int i = 0; i < result.size(); i++) {
+                bw.write(result.get(i)+" ");
+            }
+            bw.flush();
+
+        }
+    }
+}
