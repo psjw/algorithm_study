6607c199f28261cfe5434fc2adcfef276c800444
psjw <dlrudtn108@gmail.com>
2025-07-11 19:01:08 +0900

    feat(algorithm): [백준 10986] 나머지 합

---
+++ baekjoon/src/book/DoIt_알고리즘/prefixsum/Main_10986.java
@@ -0,0 +1,40 @@
+package book.DoIt_알고리즘.prefixsum;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main_10986 {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            long sum[] = new long[N + 1];
+            long cnt[] = new long[M];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 1; i <= N; i++) {
+                int now = Integer.parseInt(st.nextToken());
+                sum[i] = now + sum[i - 1];
+            }
+            long count = 0;
+            for (int i = 1; i <= N; i++) {
+                int reminder= (int)(sum[i] % M);
+                if(reminder == 0) {
+                    count++;
+                }
+                cnt[reminder]++;
+            }
+
+            for (int i = 0; i < cnt.length; i++) {
+                if(cnt[i] > 1) {
+                    long c = (cnt[i] * (cnt[i] - 1)) / 2;
+                    count += c;
+                }
+            }
+
+            bw.write(String.valueOf(count));
+            bw.flush();
+        }
+    }
+}
